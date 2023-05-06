59af3f46416ba08ca50e91fee42be23dd0ea986b
Nikita Averyanov <rac1onal@yandex.ru>
2023-05-06 16:14:42 +0300

    style: навести красоту

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Footer() {
 	return (
 		<footer className="footer page__footer">
-		<p className="footer__copyright" lang="en">
-			©2023&nbsp;Mesto&nbsp;Russia
-		</p>
-	</footer>
+			<p className="footer__copyright" lang="en">
+				©2023&nbsp;Mesto&nbsp;Russia
+			</p>
+		</footer>
 	)
 };
 
