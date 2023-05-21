df323ee1cdd11a942a52b9f269d2dbd1e4bdf9a6
Nikita Averyanov <rac1onal@yandex.ru>
2023-05-21 19:45:31 +0300

    feat: выполнить п.2 тз - удаление карточек с сервера

--- src/components/App.js
+++ src/components/App.js
@@ -72,100 +72,107 @@ function App() {
 			.catch((err) => console.log(`Получение данных по лайкам, App: ${err}`))
 	};
 
+	function handleCardDelete(card) {
+		api.deleteCard(card._id)
+			.then(() => {
+				setCards((state) => state.filter((c) => c._id !== card._id));
+			})
+			.catch((err) => console.log(`Удалние карточки с сервера, App: ${err}`))
+	};
 
-	return (
-		<CurrentUserContext.Provider value={currentUser}>
-			<div className="page">
+		return (
+			<CurrentUserContext.Provider value={currentUser}>
+				<div className="page">
 
-				<div className="page__content">
-					<Header logo={logo} />
+					<div className="page__content">
+						<Header logo={logo} />
 
-					<Main
-						cards={cards}
-						onEditAvatar={handleEditAvatarClick}
-						onEditProfile={handleEditProfileClick}
-						onAddPlace={handleAddPlaceClick}
-						onCardClick={handleCardClick}
-						onCardLike={handleCardLike}
-					// onCardDelete={handleCardDelete}
-					/>
+						<Main
+							cards={cards}
+							onEditAvatar={handleEditAvatarClick}
+							onEditProfile={handleEditProfileClick}
+							onAddPlace={handleAddPlaceClick}
+							onCardClick={handleCardClick}
+							onCardLike={handleCardLike}
+							onCardDelete={handleCardDelete}
+						/>
 
-					<Footer />
+						<Footer />
+					</div>
+
+					{/* <----     POPUP редактирования аватара    ----> */}
+					<PopupWithForm name="edit-avatar" title="Обновить аватар" buttonText="Сохранить" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
+						<input
+							className="popup__input popup__input_type_avatar"
+							type="url"
+							name="avatar"
+							id="avatar"
+							placeholder="Ссылка на изображение"
+							required=""
+						/>
+						<span className="avatar-error popup__input-error" />
+					</PopupWithForm>
+
+					{/* <----     POPUP редактирования профиля    ----> */}
+					<PopupWithForm name="edit-profile" title="Редактировать профиль" buttonText="Сохранить" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+						<input
+							className="popup__input popup__input_type_username"
+							type="text"
+							name="username"
+							id="username"
+							placeholder="Введите ваше имя"
+							minLength={2}
+							maxLength={40}
+							required=""
+						/>
+						<span className="username-error popup__input-error" />
+						<input
+							className="popup__input popup__input_type_description"
+							type="text"
+							name="description"
+							id="description"
+							placeholder="Введите информацию о себе"
+							minLength={2}
+							maxLength={200}
+							required=""
+						/>
+						<span className="description-error popup__input-error" />
+					</PopupWithForm>
+
+					{/* <----     POPUP добавления карточки    ----> */}
+					<PopupWithForm name="add-card" title="Новое место" buttonText="Создать" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+						<input
+							className="popup__input popup__input_type_title"
+							type="text"
+							name="title"
+							id="title"
+							required=""
+							placeholder="Название"
+							minLength={2}
+							maxLength={30}
+						/>
+						<span className="title-error popup__input-error" />
+						<input
+							className="popup__input popup__input_type_link"
+							type="url"
+							name="link"
+							id="link"
+							required=""
+							placeholder="Ссылка на картинку"
+						/>
+						<span className="link-error popup__input-error" />
+					</PopupWithForm>
+
+					{/* <----     POPUP подтвержения удаления карточки    ----> */}
+					<PopupWithForm name="delete-card" title="Вы уверены?" buttonText="Да" onClose={closeAllPopups} />
+
+					{/* <----     POPUP открытия карточки    ----> */}
+					<ImagePopup card={selectedCard} onClose={closeAllPopups}></ImagePopup>
+
 				</div>
+			</CurrentUserContext.Provider>
+		);
 
-				{/* <----     POPUP редактирования аватара    ----> */}
-				<PopupWithForm name="edit-avatar" title="Обновить аватар" buttonText="Сохранить" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
-					<input
-						className="popup__input popup__input_type_avatar"
-						type="url"
-						name="avatar"
-						id="avatar"
-						placeholder="Ссылка на изображение"
-						required=""
-					/>
-					<span className="avatar-error popup__input-error" />
-				</PopupWithForm>
+	};
 
-				{/* <----     POPUP редактирования профиля    ----> */}
-				<PopupWithForm name="edit-profile" title="Редактировать профиль" buttonText="Сохранить" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
-					<input
-						className="popup__input popup__input_type_username"
-						type="text"
-						name="username"
-						id="username"
-						placeholder="Введите ваше имя"
-						minLength={2}
-						maxLength={40}
-						required=""
-					/>
-					<span className="username-error popup__input-error" />
-					<input
-						className="popup__input popup__input_type_description"
-						type="text"
-						name="description"
-						id="description"
-						placeholder="Введите информацию о себе"
-						minLength={2}
-						maxLength={200}
-						required=""
-					/>
-					<span className="description-error popup__input-error" />
-				</PopupWithForm>
-
-				{/* <----     POPUP добавления карточки    ----> */}
-				<PopupWithForm name="add-card" title="Новое место" buttonText="Создать" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
-					<input
-						className="popup__input popup__input_type_title"
-						type="text"
-						name="title"
-						id="title"
-						required=""
-						placeholder="Название"
-						minLength={2}
-						maxLength={30}
-					/>
-					<span className="title-error popup__input-error" />
-					<input
-						className="popup__input popup__input_type_link"
-						type="url"
-						name="link"
-						id="link"
-						required=""
-						placeholder="Ссылка на картинку"
-					/>
-					<span className="link-error popup__input-error" />
-				</PopupWithForm>
-
-				{/* <----     POPUP подтвержения удаления карточки    ----> */}
-				<PopupWithForm name="delete-card" title="Вы уверены?" buttonText="Да" onClose={closeAllPopups} />
-
-				{/* <----     POPUP открытия карточки    ----> */}
-				<ImagePopup card={selectedCard} onClose={closeAllPopups}></ImagePopup>
-
-			</div>
-		</CurrentUserContext.Provider>
-	);
-
-};
-
-export default App;
+	export default App;
