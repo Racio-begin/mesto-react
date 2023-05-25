b21d9682bfa2e47cbd7d7f6fdf5368fd2a6cc65d
Nikita Averyanov <rac1onal@yandex.ru>
2023-05-25 20:56:45 +0300

    feat: ревью - добавить динамическое изменение года

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
 	return (
 		<footer className="footer page__footer">
 			<p className="footer__copyright" lang="en">
-				©2023&nbsp;Mesto&nbsp;Russia
+				©{new Date().getFullYear()}&nbsp;Mesto&nbsp;Russia
 			</p>
 		</footer>
 	);
